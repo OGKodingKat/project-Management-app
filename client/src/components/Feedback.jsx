7dddf99d58ae71706f9f3002ffcf73885ed1dccc
OGKodingKat <katmarie9628@gmail.com>
2025-04-17 13:22:17 -0500

    quick fix

--- client/src/components/Feedback.jsx
+++ client/src/components/Feedback.jsx
@@ -8,7 +8,7 @@ const Feedback = () => {
   return (
     <div className="feedback-empty-page">
       <div className="empty-state">
-        <img src="/img/empty.png" alt="No feedback illustration" className="feedback-img" />
+        
         <h2>There is no feedback yet.</h2>
         <p>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
         <button className="add-feedback" onClick={() => navigate('/add-feedback')}>+ Add Feedback</button>
